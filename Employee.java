b143030f2e2d527297b2aa80c7a02fe6f06f5338
saadhvi <01fe22bca104@kletech.ac.in>
2024-03-30 20:52:20 +0530

    Extended code

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    
+    int empid = 104;
+    float salary  = 7000000;
+
+  
+    void get_empid(){
+    System.out.println("Employee ID:"+empid);
+        
+    }
+void get_salary(){
+
+       System.out.println("Salary:"+salary);
+}
+}
